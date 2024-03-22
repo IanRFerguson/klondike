cf75da74c48d255d1fdf6d48aa1c5c4ed07f01e3
Ian <47256454+IanRFerguson@users.noreply.github.com>
2024-03-22 08:30:02 -0400

    set up package imports

---
+++ polar_bare/__init__.py
@@ -0,0 +1,37 @@
+import importlib
+import logging
+import os
+
+# Define project logger
+logger = logging.getLogger(__name__)
+
+_handler = logging.StreamHandler()
+_formatter = logging.Formatter("%(module)s %(levelname)s %(message)s")
+_handler.setFormatter(_formatter)
+
+logger.addHandler(_handler)
+
+# Define logging behavior
+if any([os.environ.get("TESTING"), os.environ.get("TEST")]):
+    logger.setLevel("WARNING")
+elif os.environ.get("DEBUG"):
+    logger.setLevel("DEBUG")
+    logger.debug("Debugger active...")
+else:
+    logger.setLevel("INFO")
+
+#####
+
+__all__ = []
+POLAR_OBJECTS = [
+    ("polar_bare.bigquery.bigquery", "PolarBigQuery"),
+    ("polar_bare.postgres.postgres", "PolarPostgres"),
+    ("polar_bare.redshift.redshift", "PolarRedshift"),
+    ("polar_bare.snowflake.snowflake", "PolarSnowflake"),
+]
+
+for module_, object_ in POLAR_OBJECTS:
+    try:
+        globals()[object_] = getattr(importlib.import_module(module_), object_)
+    except ImportError:
+        logger.debug(f"Could not import {module_}.{object_} ... skipping")
