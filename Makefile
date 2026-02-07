d8b9ebbf3c754cdf3b03d5864bc3de45f3a5a8b3
Ian <47256454+IanRFerguson@users.noreply.github.com>
2026-02-07 16:18:56 -0500

    ruff

--- Makefile
+++ Makefile
@@ -5,6 +5,20 @@ push:
 setup:
 	@bash run_setup.sh
 
-clean:
-	@ruff check --fix .
-	@pytest -rf tests/*
+
+### CI Checks (these run locally in the CI pipeline, but can be run locally with `make all-checks`) ###
+
+ruff:
+	@uv run ruff check --fix .
+	@uv run ruff format .
+
+pytest:
+	@uv run pytest -rf tests/*
+
+mypy:
+	@uv run mypy klondike
+
+all-checks:
+	@make ruff
+	@make pytest
+	@make mypy
--- klondike/utilities/utilities.py
+++ klondike/utilities/utilities.py
@@ -5,7 +5,9 @@ General utilites to use across all database connections
 IF_EXISTS_BEHAVIOR = ["fail", "append", "truncate", "drop"]
 
 
-def validate_if_exists_behavior(user_input: str, acceptable_values: list = IF_EXISTS_BEHAVIOR):
+def validate_if_exists_behavior(
+    user_input: str, acceptable_values: list = IF_EXISTS_BEHAVIOR
+):
     """
     Ensures that user input to `write_dataframe` function is valid
     """
